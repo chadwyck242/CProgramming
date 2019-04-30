29691c8006bdf44a1108aaecd5a7e36f0fa71604
chadwyck242 <chadwyck.codeart@gmail.com>
2019-04-30 17:20:28 -0600

    Chapter 2 Programming Project 1 finished

---
+++ ch2/date_format.c
@@ -0,0 +1,14 @@
+// convert mm /dd/yyyy to yyyymmdd format
+
+#include <stdio.h>
+
+int main(void)
+{
+    int month, day, year;
+    printf("Please enter a date (mm/dd/yyyy): ");
+    scanf("%d/%d/%d", &month, &day, &year);
+
+    printf("%d%2.2d%2.2d\n", year, month, day);
+
+    return 0;
+}
